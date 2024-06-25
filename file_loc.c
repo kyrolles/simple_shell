2d0b7b04f42bc950450c10e95e7c748ed1e79386
kyrolles <kyrollesraafat@gmail.com>
2024-06-26 02:14:00 +0300

    fix betty

--- file_loc.c
+++ file_loc.c
@@ -1,41 +1,41 @@
 #include "main.h"
 
 /**
-  * get_file_path - Get's the full path of the file
-  * @file_name: Argument name
-  *
-  * Return: The full path argument to the file
-  */
+	* get_file_path - Get's the full path of the file
+	* @file_name: Argument name
+	*
+	* Return: The full path argument to the file
+	*/
 
 char *get_file_path(char *file_name)
 {
 
-        char *path = getenv("PATH");
-				char *Full_Path;
+char *path = getenv("PATH");
+char *Full_Path;
 
-				if (file_name == NULL)
-				{
-						perror("File name is NULL");
-						return NULL;
-				}
-				
-				if (startsWithForwardSlash(file_name) && access(file_name, X_OK) == 0)
-							return (strdup(file_name));
+if (file_name == NULL)
+{
+	perror("File name is NULL");
+	return (NULL);
+}
 
-        
-				if (!path)
-        {
-                perror("Path not found");
-                return (NULL);
-        }
+if (startsWithForwardSlash(file_name) && access(file_name, X_OK) == 0)
+	return (strdup(file_name));
 
-				Full_Path = get_file_loc(path, file_name);
 
-				if(Full_Path == NULL)
-				{
-							perror("absolute path not found");
-							return(NULL);
-				}
+if (!path)
+{
+perror("Path not found");
+return (NULL);
+}
 
-        return (Full_Path);
+Full_Path = get_file_loc(path, file_name);
+
+if (Full_Path == NULL)
+{
+	perror("absolute path not found");
+	return (NULL);
+}
+
+		return (Full_Path);
 }
--- startswithforwardslash.c
+++ startswithforwardslash.c
@@ -1,16 +1,16 @@
-#include "main.h" 
+#include "main.h"
 
 /**
-  * startsWithForwardSlash - Checks if file starts with "/"
-  * @str: The filename to be checked
-  *
-  * Return: 0 if yes and 1 if NO
-  */
+	* startsWithForwardSlash - Checks if file starts with "/"
+	* @str: The filename to be checked
+	*
+	* Return: 0 if yes and 1 if NO
+	*/
 
 int startsWithForwardSlash(const char *str)
 {
-        if (str != NULL || str[0] == '/')
-                return (1);
+				if (str != NULL || str[0] == '/')
+					return (1);
 
-        return (0);
+				return (0);
 }
